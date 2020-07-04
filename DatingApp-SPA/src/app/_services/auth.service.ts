2c3846baa24c34bf5bba30e859f61983aa4eb207
Grzegorz Waluś <gwalus1404@gmail.com>
2020-07-04 17:01:15 +0200

    End of section 6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { importExpr } from '@angular/compiler/src/output/output_ast';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,8 @@ import {map} from 'rxjs/operators';
 export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;  // zmienna przechowująca odkodowany token
 
 constructor(private http: HttpClient) { }
 
@@ -18,7 +21,9 @@ constructor(private http: HttpClient) { }
       map((responce: any) => {
         const user = responce;
         if (user) {
-          localStorage.setItem('token', user.token);
+          localStorage.setItem('token', user.token); // ustawianie tekona w pamięci lokalnej klienta
+          this.decodedToken = this.jwtHelper.decodeToken(user.token); // zdekodowanie tokenu do pobrania nazwy użytkownika
+          console.log(this.decodedToken);
         }
       })
     );
@@ -26,5 +31,10 @@ constructor(private http: HttpClient) { }
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,26 +11,30 @@ export class NavComponent implements OnInit {
 
   model: any = {};  // tutaj będziemy zapisywać naszego użytkownika i hasło
 
-  constructor(private authService: AuthService) { } // wstrzyknięcie serwisu
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  // wstrzyknięcie serwisu autoryzującego, serwisu powiadomień
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged is successfully');
+      this.alertify.success('Logged is successfully'); // zastąpienie powiadomieniem zamiast wyrzuceniem na konsole
     }, error => {
-      console.log(error);
+      this.alertify.error(error); // zastąpienie powiadomieniem zamiast wyrzuceniem na konsole
     });
   } // logowanie
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token; // jeśli coś jest w tym to zwróci true, jeśli jest pusty to zwróci false
+    // const token = localStorage.getItem('token');
+    // return !!token; jeśli coś jest w tym to zwróci true, jeśli jest pusty to zwróci false
+
+    // zastąpienie kodu powyżej, poprzez wstyknięcie z serwisu autoryzującego, przeniesienie metody właśnie tam
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('Logged out');
+    this.alertify.message('Logged out'); // zastąpienie powiadomieniem zamiast wyrzuceniem na konsole
   }
 }
